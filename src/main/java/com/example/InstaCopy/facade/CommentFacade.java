7970b81b694c34897f875240d2f9e6045e2677d4
Miras <mirastilegenov518@gmail.com>
2023-08-05 21:16:03 +0600

    Creating Controllers

---
+++ src/main/java/com/example/InstaCopy/facade/CommentFacade.java
@@ -0,0 +1,16 @@
+package com.example.InstaCopy.facade;
+
+import com.example.InstaCopy.dto.CommentDTO;
+import com.example.InstaCopy.entity.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentFacade {
+    public CommentDTO commentToCommentDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUsername());
+        commentDTO.setMessage(comment.getMessage());
+        return commentDTO;
+    }
+}
---
+++ src/main/java/com/example/InstaCopy/facade/PostFacade.java
@@ -0,0 +1,21 @@
+package com.example.InstaCopy.facade;
+
+import com.example.InstaCopy.dto.PostDTO;
+import com.example.InstaCopy.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostFacade {
+    public PostDTO postToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUsername(post.getUser().getUsername());
+        postDTO.setId(post.getId());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUsersLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(post.getTitle());
+
+        return postDTO;
+    }
+}
